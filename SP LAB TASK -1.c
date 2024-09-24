aadb0d7b140ec20bafdcd2c1895cd2768442fe31
Nahin4200 <nahinworld2024@gmail.com>
2024-09-24 14:05:46 +0600

    problem -01

---
+++ SP LAB TASK -1.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main()
+{
+   int A,B;
+   scanf("%d%d",&A,&B);
+   int sum;
+   sum=A+B;
+   if(sum%2==0){
+    printf("even\n");
+   }else{
+
+   printf("odd\n");
+   }
+
+
+    return 0;
+}
